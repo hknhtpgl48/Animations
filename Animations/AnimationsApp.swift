42b4b0d6f80d359840b552ea08fe3494ba9ac519
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-01-25 17:31:52 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Hakan Hatipoğlu on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
